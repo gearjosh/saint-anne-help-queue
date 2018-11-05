9103de2caf15d8d0e5c0bdde550f7d38a5b42472
Eric Conner and Josh Gearheart <josh@>
2018-11-05 15:35:39 -0800

    add auto linting

--- webpack.config.js
+++ webpack.config.js
@@ -33,6 +33,16 @@ module.exports = {
     rules: [
       {
         test: /\.jsx?$/,
+        enforce: "pre",
+        loader: "eslint-loader",
+        exclude: /node_modules/,
+        options: {
+          emitWarning: true,
+          configFile: "./.eslintrc.json"
+        }
+      },
+      {
+        test: /\.jsx?$/,
         loader: "babel-loader",
         exclude: /node_modules/,
         options: {
